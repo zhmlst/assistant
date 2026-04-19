33a394c058a4eacd83bba370215198ae73d0bf7e
zhmlst <zhmlst.btw@gmail.com>
2026-04-19 14:10:53 +0300

    gateway/handler: hanlde errors

--- gateway/internal/handler/v1/handler.go
+++ gateway/internal/handler/v1/handler.go
@@ -3,7 +3,9 @@ package v1
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/google/uuid"
 	"github.com/zhmlst/assistant/go/lib"
@@ -30,11 +32,33 @@ func New(conversation Conversation) *handler {
 	}
 }
 
+func (h *handler) handleError(w http.ResponseWriter, err error) {
+	type Error struct {
+		Error string `json:"error"`
+	}
+
+	jerr := Error{
+		Error: err.Error(),
+	}
+
+	if err := json.NewEncoder(w).Encode(jerr); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+}
+
 func (h *handler) CreateMessage(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	var msg Message
-	_ = json.NewDecoder(r.Body).Decode(&msg)
-	_ = h.conversation.CreateMessage(r.Context(), &msg)
-	_ = json.NewEncoder(w).Encode(&msg)
+	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
+		h.handleError(w, fmt.Errorf("decode request: %w", err))
+	}
+	
+	if err := h.conversation.CreateMessage(r.Context(), &msg); err != nil {
+		h.handleError(w, err)
+	}
+
+	if err := json.NewEncoder(w).Encode(&msg); err != nil {
+		h.handleError(w, fmt.Errorf("encode response: %w", err))
+	}
 }
